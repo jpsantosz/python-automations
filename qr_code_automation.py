974f18188daf341d4dd9f296a5f184480f24bec3
jpsantosz <jpsantosc1503@gmail.com>
2024-11-19 21:14:15 -0300

    adicionando comentários na automação de geração de qrcode

--- qr_code_automation.py
+++ qr_code_automation.py
@@ -1,12 +1,16 @@
+# Importando a biblioteca qrcode para geração de QRcode (A biblioteca Pillow é necessária para que o módulo qrcode funcione corretamente ao gerar imagens.)
 import qrcode as qr
 
+# Definindo as especificações do QRcode a ser gerado
 qr_code = qr.QRCode(version=1,
                     error_correction=qr.constants.ERROR_CORRECT_L,
                     box_size=50,
                     border=1)
 
+# Incluindo o conteúdo do QRcode
 qr_code.add_data("https://pt.wikipedia.org/wiki/Club_de_Regatas_Vasco_da_Gama")
 qr_code.make(fit=True)
 
+# Gerando e salvando a imagem do QRcode
 img = qr_code.make_image(fill_color="black", back_color="white")
 img.save("qr_code.png")
